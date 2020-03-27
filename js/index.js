4e06b55aa5f25800fd1fde8fbb86eda9bb094312
Idowu <iarifayan@russelsmith.local>
2020-03-27 16:29:05 +0100

    SVG animated text updated

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 let textPath = document.querySelector("#text-path");
+let titles = document.querySelectorAll(".anim");
 
 function updateTextPathOffset(offset) {
   textPath.setAttribute("startOffset", offset);
@@ -11,3 +12,17 @@ function onScroll() {
 }
 
 window.onscroll = onScroll;
+
+let observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting == true) {
+      entry.target.classList.add("animate");
+    } else {
+      entry.target.classList.remove("animate");
+    }
+  });
+});
+
+titles.forEach(title => {
+  observer.observe(title);
+});
